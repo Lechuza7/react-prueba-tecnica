b6525bf62f18433fe41965457b53172d56110f40
Marc Cuesta <marc.cuestacarabasa@gmail.com>
2023-04-30 18:23:24 +0200

    First API data fetched

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+
+const CAT_ENDPOINT_RANDOM_FACT = "https://catfact.ninja/fact"
+
+export function App() {
+  const [fact, setFact] = useState("lorem ipsum cat fact");
+
+  useEffect(() => {
+    fetch(CAT_ENDPOINT_RANDOM_FACT)
+    .then(res => res.json())
+    .then(data => setFact(data.fact))
+  }, [])
+
+  return (
+    <main>
+    <h1>App de gatitos</h1>
+    <p>{fact}</p>
+    </main>
+  )
+}
