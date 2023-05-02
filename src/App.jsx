9d59be86f6882d9781476ad17f0f062aa1513694
Marc Cuesta <marc.cuestacarabasa@gmail.com>
2023-05-02 17:44:33 +0200

    CSS & minors

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,45 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
+import "./App.css";
 
-const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
-const CAT_ENDPOINT_IMAGE_URL = 'https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true'
-const CAT_PREFIX_IMAGE_URL = 'https://cataas.com'
+const CAT_ENDPOINT_RANDOM_FACT = "https://catfact.ninja/fact";
+const CAT_PREFIX_IMAGE_URL = "https://cataas.com";
 
-export function App () {
-  const [fact, setFact] = useState('lorem ipsum cat fact')
-  const [imageUrl, setImageUrl] = useState('')
+export function App() {
+  const [fact, setFact] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
 
   useEffect(() => {
     fetch(CAT_ENDPOINT_RANDOM_FACT)
       .then((res) => res.json())
       .then((data) => {
-        const { fact } = data
-        setFact(fact)
-        const firstWord = fact.split(' ')[0]
+        const { fact } = data;
+        setFact(fact);
+      });
+  }, []);
 
-        fetch(CAT_ENDPOINT_IMAGE_URL)
-        .then(res => res.json())
-        .then(response => {
-          const { url } = response
-          setImageUrl(url)
-        })
-      })
-  }, [])
+  useEffect(() => {
+    if (!fact) return;
+
+    const firstWord = fact.split(" ")[0];
+
+    fetch(`https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`)
+      .then((res) => res.json())
+      .then((response) => {
+        const { url } = response;
+        setImageUrl(url);
+      });
+  }, [fact]);
 
   return (
     <main>
       <h1>App de gatitos</h1>
       {fact && <p>{fact}</p>}
-      {imageUrl && <img src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`} alt={`Image extracted using the first three word of ${fact}`} />}
+      {imageUrl && (
+        <img
+          src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`}
+          alt={`Image extracted using the first three word of ${fact}`}
+        />
+      )}
     </main>
-  )
+  );
 }
